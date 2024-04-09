bd0df9fdec6c8cb1c4bb93bb7cce70a265e6467a
Tyler Lui <114965892+tylermlui@users.noreply.github.com>
2024-04-08 18:57:42 -0700

    Update server.py

--- project-backend/server.py
+++ project-backend/server.py
@@ -11,7 +11,7 @@ def apiCall():
     url = "https://api.themoviedb.org/3/authentication"
 
     headers = {"accept": "application/json",
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MWYyYmQxZTgwNTMzNTdlN2ZiN2NlZWI0YTIzN2IxYyIsInN1YiI6IjY1YzE4ZmQ5YTA2NjQ1MDE2MTVkODM0YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MylPZUvgDbWY9wheuA6PfkzGWovHcS4LaZs-q8Gju-E"}
+            "Authorization": "Bearer TOKEN"}
 
     response = requests.get(url, headers=headers)
 

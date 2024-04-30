39aa5b64a0d7b14bf646128ace654642e086aa2c
sdelgado6625 <104951047+sdelgado6625@users.noreply.github.com>
2024-04-30 00:12:16 -0700

    test

--- filmfinder/src/components/Recommended_Page/MainContent.js
+++ filmfinder/src/components/Recommended_Page/MainContent.js
@@ -4,34 +4,41 @@ import './RecommendedMainContent.css';
 
 function MainContent() {
     const [searchResults, setSearchResults] = useState(null);
-    const [searchTerm, setSearchTerm] = useState('');
 
-    const handleChange = (e) => {
-        setSearchTerm(e.target.value);
-    };
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const urlParams = new URLSearchParams(window.location.search);
+                const genre = urlParams.get('genre');
+                const year = urlParams.get('year');
+                const rating = urlParams.get('rating');
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post('http://127.0.0.1:5000/recommendation', formData);
-            setSearchResults(response.data.results); // Assuming the response contains a 'results' field
-        } catch (error) {
-            console.error('Error fetching search results:', error);
-        }
-    };
+                console.log('Received genre:', genre);
+                console.log('Received year:', year);
+                console.log('Received rating:', rating);
+
+                const request = {
+                    genre: genre ,
+                    year: year,
+                    rating: rating
+                };
+
+                const response = await axios.post('http://127.0.0.1:5000/recommendation', request);
+
+                console.log('Received response:', response.data); // Log the backend's response
+
+                setSearchResults(response.data.results); // Assuming the response contains a 'results' field
+            } catch (error) {
+                console.error('Error fetching search results:', error);
+            }
+        };
+
+        fetchData();
+    }, []); // Run once on component mount
 
     return (
         <main>
-            <h2 className='title'>Search Results for: {searchTerm}</h2>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    value={searchTerm}
-                    onChange={handleChange}
-                    placeholder="Enter movie title"
-                />
-                <button type="submit">Search</button>
-            </form>
+            <h2 className='title'>Search Results</h2>
             <div className="movie-list">
                 {searchResults ? (
                     searchResults.map(movie => (
@@ -54,4 +61,3 @@ function MainContent() {
 }
 
 export default MainContent;
-
